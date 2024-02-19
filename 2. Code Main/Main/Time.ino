b00dd31863dbd88b0affdd50fca9e0a20856a460
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-02-19 14:43:26 +0700

    not yet set time via button

---
+++ 2. Code Main/Main/Time.ino
@@ -0,0 +1,37 @@
+void printTime() {
+  lcd.setCursor(0,0);
+  lcd.print(now.day(),DEC); lcd.print("/"); lcd.print(now.month(),DEC); lcd.print("/"); lcd.print(now.year(),DEC); lcd.print("|");
+  
+  lcd.setCursor(10,0);
+  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+
+  lcd.setCursor(0,1);
+  lcd.print(now.hour(),DEC); lcd.print(":"); lcd.print(now.minute(),DEC); lcd.print(":"); lcd.print(now.second(),DEC); lcd.print("   ");
+
+  if( now.hour() != currentHour ){
+    Ring(2,100);
+    currentHour = now.hour();
+  }
+}
+
+void turnOnOffDevice(int hourOn, int minuteOn, int hourOff, int minuteOff){
+  if( now.hour() == hourOn && now.minute() == minuteOn ){
+    Ring(1,100);
+    digitalWrite(LIGHT1, 1);
+    digitalWrite(LIGHT2, 1);
+  }
+  else if( now.hour() == hourOff && now.minute() == minuteOff ){
+    Ring(1,100);
+    digitalWrite(LIGHT1, 0);
+    digitalWrite(LIGHT2, 0);
+  }
+}
+
+void settingTime(){
+  lcd.setCursor(3, 0);
+  lcd.print("SET TIMER: ");
+}
+
+
+
+
