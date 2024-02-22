9c4401030e2363e193454f832a7e6782748c0351
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-02-22 16:10:38 +0700

    Create TestBlynk.ino

---
+++ 1. Code Test/TestBlynk/TestBlynk.ino
@@ -0,0 +1,26 @@
+#define BLYNK_TEMPLATE_ID "TMPL6XAznY3Lo"
+#define BLYNK_TEMPLATE_NAME "SMART HOME"
+#define BLYNK_AUTH_TOKEN "DTN0jSU9VzT6GvEJr8ULeTh4nWK8ChY1"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <string.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "MINH KHA";
+char pass[] = "0855508877";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  Blynk.virtualWrite(V5, 17);
+  delay(2000);
+}
